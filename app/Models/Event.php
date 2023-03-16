e577fa7d5fc378909c1b8644d17622bdd12db36d
LukasC0D <kaupasa666@gmail.com>
2023-03-16 13:14:18 +0200

    created event model

---
+++ app/Models/Event.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'pavadinimas',
+        'data',
+        'aprašymas',
+        'renginio vieta'
+    ];
+    protected $hidden = [
+        'timestamps',
+    ];
+}
---
+++ database/migrations/2023_03_16_110140_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('pavadinimas');
+            $table->float('data');
+            $table->string('aprašymas');
+            $table->string('renginio_vieta');
+            $table->foreignId('user_id')->nullable()->constrained('countries')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
